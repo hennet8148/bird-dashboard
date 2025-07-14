21aec72c7698ce9fe557781592dcc32e051350e3
Chuck Hennet <dba.hennet@gmail.com>
2025-07-14 13:08:10 -0400

    s

--- template_bird_page.php
+++ template_bird_page.php
@@ -8,6 +8,12 @@
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-white text-gray-800">
+
+  <!-- Inject species code early so calendar.php can pick it up -->
+  <script>
+    window.speciesCode = "{{species_code}}";
+  </script>
+
   <div class="max-w-5xl mx-auto p-6">
     <a href="javascript:history.back()" class="text-sm text-blue-600 hover:underline">&larr; Return to Previous Page</a>
 
@@ -30,10 +36,6 @@
     </div>
   </div>
 
-  <!-- Inject species code for use in calendar highlighting -->
-  <script>
-    window.speciesCode = "{{species_code}}";
-  </script>
 </body>
 </html>
 
