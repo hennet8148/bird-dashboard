d5fcb3ba051d251d06637bdba812ed16696ee308
Chuck Hennet <dba.hennet@gmail.com>
2025-07-22 08:59:15 -0400

    s

--- cards/card-total-detections.php
+++ cards/card-total-detections.php
@@ -1,5 +1,5 @@
-<div class="bg-white rounded shadow p-4 text-center">
-  <h3 class="text-sm text-gray-600 mb-1 font-semibold">Total Detections</h3>
-  <div id="totalSightings" class="text-2xl font-bold text-black">—</div>
+<div class="bg-white p-4 rounded shadow text-center">
+  <h3 class="text-md font-semibold text-gray-700 mb-1">Total Detections</h3>
+  <p id="totalSightings" class="text-2xl font-bold text-blue-700">Loading...</p>
 </div>
 
